041fa24fc28e2ee65f7663ca881552c72f17c226
Cezary Troska <CTroska@noblesys.com>
2022-11-11 15:33:29 +0100

    Switch extension to adding button on Scholar

--- scripts/content.js
+++ scripts/content.js
@@ -1,47 +1,23 @@
-// Copyright 2022 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+class ArticleNode {
+    constructor(name) {
+        this.name = name;
+        this.favoriteFlavors = [];
+    }
 
-const article = document.querySelector("article");
+    addArticle(flavor) {
+        this.favoriteFlavors.push(flavor);
+    }
+}
+*/
+const results = document.getElementsByClassName("gs_ggs gs_fl")
 
-// `document.querySelector` may return null if the selector doesn't match anything.
-if (article) {
-    const text = article.textContent;
-    /**
-     * Regular expression to find all "words" in a string.
-     *
-     * Here, a "word" is a sequence of one or more non-whitespace characters in a row. We don't use the
-     * regular expression character class "\w" to match against "word characters" because it only
-     * matches against the Latin alphabet. Instead, we match against any sequence of characters that
-     * *are not* a whitespace characters. See the below link for more information.
-     *
-     * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
-     */
-    const wordMatchRegExp = /[^\s]+/g;
-    const words = text.matchAll(wordMatchRegExp);
-    // matchAll returns an iterator, convert to array to get word count
-    const wordCount = [...words].length;
-    const readingTime = Math.round(wordCount / 200);
-    const badge = document.createElement("p");
-    // Use the same styling as the publish information in an article's header
-    badge.classList.add("color-secondary-text", "type--caption");
-    badge.textContent = `⏱️ ${readingTime} min read`;
+if (results) {
+    for (let i = 0; i < results.length; i++) {
+        const create_graph = document.createElement("button");
+        create_graph.textContent = `project button`;
 
-    // Support for API reference docs
-    const heading = article.querySelector("h1");
-    // Support for article docs with date
-    const date = article.querySelector("time")?.parentNode;
+        results[i].insertAdjacentElement("beforeend", create_graph);
+    }
 
-    // https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentElement
-    (date ?? heading).insertAdjacentElement("afterend", badge);
 }
