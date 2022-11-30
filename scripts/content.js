9dcd9b4714bfee858b68f46aff6e8462be2414d7
AntoniDabrowski <38124281+AntoniDabrowski@users.noreply.github.com>
2022-11-30 21:31:40 +0100

    Scraping of a publication title form a Google Scholar page

--- scripts/content.js
+++ scripts/content.js
@@ -10,38 +10,46 @@ class ArticleNode {
     }
 }
 */
-const pdf_results = document.getElementsByClassName("gs_ggs gs_fl")
+// navigates to sections with both description of the article and link to its pdf
+const publications = document.getElementsByClassName("gs_r gs_or gs_scl");
 
 
-if (pdf_results) {
-    for (let i = 0; i < pdf_results.length; i++) {
-        const create_graph = document.createElement("button");
-        create_graph.className = "collapsible";
-        create_graph.textContent = `project button`;
+if (publications) {
+    for (let i = 0; i < publications.length; i++) {
+//        checks whether founded publication has link to the pdf
+        const publication_pdf = publications[i].getElementsByClassName("gs_ggs gs_fl");
 
-        const drawing_board = document.createElement("div");
-        drawing_board.innerHTML = "<p>Lorem ipsum...</p>";
-        drawing_board.className = "content";
-        drawing_board.style.display = "none"
+        if (publication_pdf.length === 1) {
+//            location of class containing article title
+            const publication_info = publications[i].getElementsByClassName("gs_rt");
+            const location = publication_info[0].children.length
+            const publication_title = publication_info[0].children[location-1]['innerText'];
 
-        create_graph.addEventListener("click", function () {
-            this.classList.toggle("active");
-            if (drawing_board.style.display === "block") {
-                drawing_board.style.display = "none";
-            } else {
-                drawing_board.style.display = "block";
-            }
-        });
+            const create_graph = document.createElement("button");
+            create_graph.className = "collapsible";
+            create_graph.textContent = `project button`;
 
-        let links_of_result;
-        let search_result_box;
+            const drawing_board = document.createElement("div");
+            drawing_board.innerHTML = "<p>"+publication_title+"</p>";
+            drawing_board.className = "content";
+            drawing_board.style.display = "none";
 
-        pdf_results[i].insertAdjacentElement("beforeend", create_graph);
-        search_result_box = pdf_results[i].parentNode;
-        links_of_result = search_result_box.getElementsByClassName('gs_fl');
-        links_of_result[1].insertAdjacentElement('afterend', drawing_board)
+            create_graph.addEventListener("click", function () {
+                this.classList.toggle("active");
+                if (drawing_board.style.display === "block") {
+                    drawing_board.style.display = "none";
+                } else {
+                    drawing_board.style.display = "block";
+                }
+            });
 
+            let links_of_result;
+            let search_result_box;
 
+            publication_pdf[0].insertAdjacentElement("beforeend", create_graph);
+            search_result_box = publication_pdf[0].parentNode;
+            links_of_result = search_result_box.getElementsByClassName('gs_fl');
+            links_of_result[1].insertAdjacentElement('afterend', drawing_board)
+        }
     }
-
 }
