a8ad65f9d4b7f91562e52ab4acba149d597237bf
CTroska <Cezary.Troska@Alvaria.com>
2022-12-07 10:27:07 +0100

    Add Plotly to the project

--- scripts/content.js
+++ scripts/content.js
@@ -10,6 +10,8 @@ class ArticleNode {
     }
 }
 */
+var Plotly = require('plotly.js-strict-dist')
+
 const pdf_results = document.getElementsByClassName("gs_ggs gs_fl")
 
 
@@ -20,8 +22,9 @@ if (pdf_results) {
         create_graph.textContent = `project button`;
 
         const drawing_board = document.createElement("div");
-        drawing_board.innerHTML = "<p>Lorem ipsum...</p>";
+        //drawing_board.innerHTML = "<p>Lorem ipsum...</p>";
         drawing_board.className = "content";
+        drawing_board.id = "graph"
         drawing_board.style.display = "none"
 
         create_graph.addEventListener("click", function () {
@@ -36,11 +39,41 @@ if (pdf_results) {
         let links_of_result;
         let search_result_box;
 
+
         pdf_results[i].insertAdjacentElement("beforeend", create_graph);
         search_result_box = pdf_results[i].parentNode;
         links_of_result = search_result_box.getElementsByClassName('gs_fl');
         links_of_result[1].insertAdjacentElement('afterend', drawing_board)
 
+        Plotly.newPlot('graph', [{
+            x: [1, 2, 3],
+            y: [2, 1, 3]
+        }], {
+            sliders: [{
+                pad: {t: 30},
+                currentvalue: {
+                    xanchor: 'right',
+                    prefix: 'color: ',
+                    font: {
+                        color: '#888',
+                        size: 20
+                    }
+                },
+                steps: [{
+                    label: 'red',
+                    method: 'restyle',
+                    args: ['line.color', 'red']
+                }, {
+                    label: 'green',
+                    method: 'restyle',
+                    args: ['line.color', 'green']
+                }, {
+                    label: 'blue',
+                    method: 'restyle',
+                    args: ['line.color', 'blue']
+                }]
+            }]
+        });
 
     }
 
