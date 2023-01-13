b35e46c25da63f483fdf6646bb43900a8feddedd
CTroska <Cezary.Troska@Alvaria.com>
2023-01-13 15:09:34 +0100

    Create better text for graphs

--- extension/scripts/graphs.js
+++ extension/scripts/graphs.js
@@ -1,31 +1,45 @@
 var Plotly = require('plotly.js-strict-dist')
 
+function create_text_for_graph(nodes) {
+    let result = [];
+
+    for (let i = 0; i < nodes.length; i++) {
+        let node_dict = nodes[i]
+        result.push(`<a href="${node_dict['url']}">${node_dict['title']}</a>` +
+            `<br>Authors: ${node_dict['authors']}<br>#citations: ${node_dict['num_publications']}`)
+    }
+
+    return result;
+}
+
 export function create_graph_on_scholar_result(divid, json) {
     console.debug(json)
     let x = [];
     let y = [];
     let point_size = [];
-    for (let key in json.layout){
+    for (let key in json.layout) {
         x.push(json.layout[key][0])
         y.push(json.layout[key][1])
     }
-    for (let i = 0; i < json.nodes.length; i++){
+    for (let i = 0; i < json.nodes.length; i++) {
         point_size.push(json.nodes[i]['num_publications'])
     }
+
+    const text = create_text_for_graph(json.nodes)
 
     console.debug(x)
     console.debug(y)
     console.debug(point_size)
 
 
-    Plotly.newPlot(divid,[{
-        x:x,
-        y:y,
-        text: [`A<br>size: 40`, 'B<br>size: 60', 'C<br>size: 80', 'D<br>size: 100', 'E<br>size: 80', 'E<br>size: 80'],
+    Plotly.newPlot(divid, [{
+        x: x,
+        y: y,
+        text: text,
         mode: 'markers',
         marker: {
             size: point_size,
-            sizemode:'area'
+            sizemode: 'area'
         }
     }])
 }
