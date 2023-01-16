8b7e8df0bbba82e61a6b20c14ccbfe86ad0a5b62
BlueDespero <32748309+BlueDespero@users.noreply.github.com>
2023-01-16 12:57:22 +0100

    Megre issues/issue_8 to develop
    Closes #8

--- extension/webpack.config.js
+++ extension/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'production',
     entry: path.resolve(__dirname, 'scripts/content.js'), //'.scripts/content.js',
     output: {
         filename: 'content.js',
