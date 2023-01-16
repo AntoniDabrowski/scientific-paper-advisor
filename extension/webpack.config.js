4815b69402bc975bdafa6cc91718083abd0121d2
BlueDespero <cezarytroska@gmail.com>
2023-01-16 12:56:33 +0100

    Chrome won't accept development mode, switching to production.

--- extension/webpack.config.js
+++ extension/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: path.resolve(__dirname, 'scripts/content.js'), //'.scripts/content.js',
     output: {
         filename: 'content.js',
