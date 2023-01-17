72d9fb4198eea8441fe25614aa77923b6e89a698
CTroska <Cezary.Troska@Alvaria.com>
2023-01-17 15:27:46 +0100

    Add pdfparser endpoint.

--- backend/frquestions/views.py
+++ backend/frquestions/views.py
@@ -1,6 +1,26 @@
-from django.http import HttpResponse, HttpRequest
+import os
+import tempfile
+from pathlib import Path
+
+import requests
+from django.http import HttpResponse, HttpRequest, JsonResponse
+from science_parse_api.api import parse_pdf
 
 
 def index(request: HttpRequest):
     print(request.method)
     return HttpResponse("Hello, world. You're at the frquestion index.")
+
+
+def parsepdf(request: HttpRequest):
+    host = 'pdfparser'
+    port = os.getenv('PDFPARSER_PORT')
+
+    with tempfile.NamedTemporaryFile() as fp:
+        url = request.GET.get('pdfurl')
+        response = requests.get(url)
+        fp.write(response.content)
+
+        output_dict = parse_pdf(host, Path(fp.name), port=port)
+
+    return JsonResponse(output_dict)
