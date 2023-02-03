bc8c6fc180c069701b646e6f044536e412781eec
AntoniDabrowski <38124281+AntoniDabrowski@users.noreply.github.com>
2023-02-03 02:31:09 +0100

    Adding radio buttons to popup - http/https

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,19 +1,19 @@
 window.onload = function() {
-    chrome.storage.local.get(["HOST_IP"]).then((result) => {
+    chrome.storage.local.get(["HOST_IP"]).then((result_IP) => {
         const myIP = document.getElementById("myIP");
-        if (result) {
-            myIP.value = result["HOST_IP"];
-        }
-        else {
+        if (Object.keys(result_IP).length === 0) {
             chrome.storage.local.set({ 'HOST_IP': "34.118.92.99"});
             myIP.value = "34.118.92.99";
         }
+        else {
+            myIP.value = result_IP["HOST_IP"];
+        }
 
-        if (myIP.value === "34.118.92.99") {
+        if (myIP.value == "34.118.92.99") {
             const server_button = document.getElementById("server_button");
             server_button.checked = true;
         }
-        else if (myIP.value === "127.0.0.1") {
+        else if (myIP.value == "127.0.0.1") {
             const local_button = document.getElementById("local_button");
             local_button.checked = true;
         }
@@ -21,6 +21,35 @@ window.onload = function() {
             const custom_button = document.getElementById("custom_button");
             custom_button.checked = true;
         }
+
+        chrome.storage.local.get(["PROTOCOL"]).then((result) => {
+            const http_protocol = document.getElementById("http");
+            const https_protocol = document.getElementById("https");
+            if (Object.keys(result).length === 0) {
+                const local_button = document.getElementById("local_button");
+                const server_button = document.getElementById("server_button");
+                if (local_button.checked) {
+                    chrome.storage.local.set({ 'PROTOCOL': 'http://' });
+                    http_protocol.checked = true;
+                }
+                else if (server_button.checked) {
+                    chrome.storage.local.set({ 'PROTOCOL': 'https://' });
+                    https_protocol.checked = true;
+                }
+                else {
+                    chrome.storage.local.set({ 'PROTOCOL': 'https://' });
+                    https_protocol.checked = true;
+                }
+            }
+            else {
+                if (result['PROTOCOL'] === http_protocol.value) {
+                    http_protocol.checked = true;
+                }
+                else {
+                    https_protocol.checked = true;
+                }
+            }
+        });
     });
 };
 
@@ -33,18 +62,35 @@ const btn = document.getElementById("btn");
 btn.addEventListener('click', function(){
     var host = document.getElementById("myIP").value;
     if (checkIfValidIP(host)) {
+        const http_protocol = document.getElementById("http");
+        const https_protocol = document.getElementById("https");
+        let current_protocol;
+        if (http_protocol.checked) {
+            current_protocol = http_protocol.value;
+            chrome.storage.local.set({ 'PROTOCOL': http_protocol.value });
+        }
+        else {
+            current_protocol = https_protocol.value;
+            chrome.storage.local.set({ 'PROTOCOL': https_protocol.value });
+        }
+
         chrome.storage.local.set({ 'HOST_IP': host });
-        alert("Host IP set to: "+ host);
+        alert("Host IP set to: "+ host + "\nWith protocol: " + current_protocol);
     }
     else {
         alert(host + " is not valid IP adress");
     }
 });
 
+
+const http_protocol = document.getElementById("http");
+const https_protocol = document.getElementById("https");
+
 const server_button = document.getElementById("server_button");
 server_button.addEventListener('click', function(){
     const myIP = document.getElementById("myIP");
     myIP.value = server_button.value;
+    https_protocol.checked = true;
 });
 
 
@@ -52,6 +98,7 @@ const local_button = document.getElementById("local_button");
 local_button.addEventListener('click', function(){
     const myIP = document.getElementById("myIP");
     myIP.value = local_button.value;
+    http_protocol.checked = true;
 });
 
 const custom_button = document.getElementById("custom_button");
--- extension/scripts/settings.js
+++ extension/scripts/settings.js
@@ -1,12 +1,13 @@
 export let PROTOCOL = 'https://'
-export let BACKEND_SERVER_URL = `${PROTOCOL}34.118.92.99:8000`;
+export let IP = '34.118.92.99'
+export let BACKEND_SERVER_URL = `${PROTOCOL}${IP}:8000`;
 
 export let FURTHER_RESEARCH_ENDPOINT = `${BACKEND_SERVER_URL}/frquestions/pdfparser`;
 export let ARTICLE_GRAPH_ENDPOINT = `${BACKEND_SERVER_URL}/articlegraph`;
 export let ARTICLE_GRAPH_LEFT_EXPAND = `${ARTICLE_GRAPH_ENDPOINT}/expandleft`;
 export let ARTICLE_GRAPH_RIGHT_EXPAND = `${ARTICLE_GRAPH_ENDPOINT}/expandright`;
 
-function update_endpoints(IP){
+function update_endpoints(PROTOCOL, IP){
     BACKEND_SERVER_URL = PROTOCOL + IP + ':8000';
     FURTHER_RESEARCH_ENDPOINT = `${BACKEND_SERVER_URL}/frquestions/pdfparser`;
     ARTICLE_GRAPH_ENDPOINT = `${BACKEND_SERVER_URL}/articlegraph`;
@@ -16,19 +17,39 @@ function update_endpoints(IP){
 
 window.onload = function() {
     chrome.storage.local.get(["HOST_IP"]).then((result) => {
-        const myIP = document.getElementById("myIP");
-        if (result) {
-            update_endpoints(result["HOST_IP"])
+        if (Object.keys(result).length === 0) {
+            IP = "34.118.92.99";
+            chrome.storage.local.set({ 'HOST_IP': "34.118.92.99"});
+            update_endpoints(PROTOCOL, IP);
         }
         else {
-            chrome.storage.local.set({ 'HOST_IP': "34.118.92.99"});
-            update_endpoints('34.118.92.99');
+            IP = result["HOST_IP"];
+            update_endpoints(PROTOCOL, IP);
+        }
+    });
+
+    chrome.storage.local.get(["PROTOCOL"]).then((result) => {
+        if (Object.keys(result).length === 0) {
+            PROTOCOL = "https://";
+            chrome.storage.local.set({ 'PROTOCOL': "https://"});
+            update_endpoints(PROTOCOL, IP);
+        }
+        else {
+            PROTOCOL = result["PROTOCOL"];
+            update_endpoints(PROTOCOL, IP);
+
         }
     });
 };
 
 chrome.storage.onChanged.addListener((changes, namespace) => {
     for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
-        update_endpoints(newValue);
+        if (key === "PROTOCOL") {
+            PROTOCOL = newValue;
+        }
+        else if (key === "HOST_IP") {
+            IP = newValue;
+        }
     }
+    update_endpoints(PROTOCOL, IP);
 });
