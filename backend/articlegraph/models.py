8eb349b093b62e2363e800d3f1fee1643c145f56
BlueDespero <cezarytroska@gmail.com>
2023-02-09 16:19:20 +0100

    Keep authors lists in the models

--- backend/articlegraph/models.py
+++ backend/articlegraph/models.py
@@ -1,3 +1,5 @@
+import json
+
 from django.db import models
 from picklefield import PickledObjectField
 
@@ -6,6 +8,12 @@ class ScholarlyPublication(models.Model):
     title = models.CharField(max_length=512)
     authors = models.CharField(max_length=512)
     publication = PickledObjectField()
+
+    def set_authors(self, x):
+        self.authors = json.dumps(x)
+
+    def get_authors(self):
+        return json.loads(self.authors)
 
 
 class CitationReferences(models.Model):
@@ -17,3 +25,9 @@ class FailedPublicationGrab(models.Model):
     title = models.CharField(max_length=512)
     authors = models.CharField(max_length=512)
     attemptdate = models.DateField(auto_now=True)
+
+    def set_authors(self, x):
+        self.authors = json.dumps(x)
+
+    def get_authors(self):
+        return json.loads(self.authors)
