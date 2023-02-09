b4da7cd5a245ff3b3ca4b487afbf66a84df70198
BlueDespero <cezarytroska@gmail.com>
2023-02-09 16:19:46 +0100

    Aply new methods of authors checking

--- backend/articlegraph/views.py
+++ backend/articlegraph/views.py
@@ -23,6 +23,11 @@ from .models import ScholarlyPublication, CitationReferences, FailedPublicationG
 from .popularity_prediction.model import predict
 
 
+def normalize_authors_list(authors_list: List[str]):
+    authors_list = [unidecode(author).lower().split()[-1] for author in authors_list]
+    return authors_list
+
+
 class PublicationWithID:
     def __init__(self, pk=None, idx: int = None, publication: Publication = None):
         if pk is not None:
@@ -41,18 +46,17 @@ class PublicationWithID:
 
 
 def add_publication_to_database(publication: Publication, cites: ScholarlyPublication = None):
-    authors = sorted(publication['bib'].get('author'))
-    authors_mash = "".join([unidecode(a).lower() for a in authors])
+    authors = normalize_authors_list(publication['bib'].get('author'))
 
     try:
         record = ScholarlyPublication.objects.get(title=publication['bib'].get('title').lower())
     except ScholarlyPublication.DoesNotExist:
         record = ScholarlyPublication.objects.create(title=publication['bib'].get('title').lower(),
-                                                     authors=authors_mash,
+                                                     authors=authors,
                                                      publication=publication)
     else:
         record.title = publication['bib'].get('title').lower()
-        record.authors = authors_mash
+        record.authors = authors
         record.publication = publication
         record.save()
 
@@ -264,6 +268,17 @@ def articles_match(publication_dict, authors, title):
     return title == publication_dict['title'] and authors == publication_authors
 
 
+def authors_check_out(first_authors_set: List[str], other_authors_set: List[str]):
+    def intersection(lst1, lst2):
+        lst3 = [value for value in lst1 if value in lst2]
+        return lst3
+
+    first_authors_set = normalize_authors_list(first_authors_set)
+    other_authors_set = normalize_authors_list(other_authors_set)
+
+    return intersection(first_authors_set, other_authors_set) != []
+
+
 def find_article(title: str, authors: List[str]) -> PublicationWithID:
     """
     :param title: Title of the article.
@@ -273,8 +288,7 @@ def find_article(title: str, authors: List[str]) -> PublicationWithID:
         have been found in Google Scholar.
     """
     # Check the database for stored data
-    authors = sorted([author.replace('.', '') for author in authors])
-    authors_mash = "".join([unidecode(a).lower() for a in authors])
+    authors = normalize_authors_list(authors)
     title = title.lower()
 
     timeout_failed_search = date.today() - timedelta(days=int(os.getenv('TIMEOUT_OF_FAILED_SEARCH_IN_DAYS')))
@@ -282,37 +296,45 @@ def find_article(title: str, authors: List[str]) -> PublicationWithID:
     if ScholarlyPublication.objects.filter(title=title).exists():
         # Publication found. Get from the database
         print("Record for publication {} retrieved from the database.".format(title))
+
         db_records = ScholarlyPublication.objects.get(title=title)
         if isinstance(db_records, QuerySet):
-            raise RuntimeError('Database contain more than one result with this title and authors.')
+            for potential_article in db_records:
+                potential_authors = potential_article.authors
+                if authors_check_out(authors, potential_authors):
+                    return PublicationWithID(idx=potential_article.pk, publication=potential_article.publication)
         else:
-            return PublicationWithID(idx=db_records.pk, publication=db_records.publication)
+            if authors_check_out(authors, db_records.authors):
+                return PublicationWithID(idx=db_records.pk, publication=db_records.publication)
+
     elif FailedPublicationGrab.objects.filter(title=title,
                                               attemptdate__gte=timeout_failed_search).exists():
         # We already looked for this article and failed
         raise RuntimeError('Article {} is recorded in the database as a failed searched.'.format(title))
-    else:
-        # query need to reflect what we put into the Google Scholar search bar.
-        print("Looking for article {} in scholarly.".format(title))
-        query = title + " " + " ".join(["author:\"{}\"".format(author) for author in authors])
-        publication_generator = scholarly.search_pubs(query=query)
 
-        counter = 0
-        for publication in publication_generator:
-            # Use api to find an article with matching title and authors list.
+    # query need to reflect what we put into the Google Scholar search bar.
 
-            if publication['bib'].get('title', 'NOT FOUND').lower() == title.lower():
-                # Save article to db for reuse.
-                return add_publication_to_database(publication=publication)
-            else:
-                add_publication_to_database(publication=publication)  # Save any found articles
+    print("Looking for article {} in scholarly.".format(title))
+    query = title + " " + " ".join(["author:\"{}\"".format(author) for author in authors])
+    publication_generator = scholarly.search_pubs(query=query)
 
-            counter += 1
-            if counter == os.getenv('LIMIT_OF_ARTICLES_TO_SEARCH_THROUGH'):
-                break
+    counter = 0
+    for publication in publication_generator:
+        # Use api to find an article with matching title and authors list.
+
+        if publication['bib'].get('title', 'NOT FOUND').lower() == title.lower() \
+                and authors_check_out(authors, publication['bib'].get('authors', 'NOT FOUND')):
+            # Save article to db for reuse.
+            return add_publication_to_database(publication=publication)
+        else:
+            add_publication_to_database(publication=publication)  # Save any found articles
+
+        counter += 1
+        if counter == int(os.getenv('LIMIT_OF_ARTICLES_TO_SEARCH_THROUGH')):
+            break
 
     # Matching article wasn't found.
-    FailedPublicationGrab.objects.update_or_create(title=title, authors=authors_mash)  # Record failed search attempt
+    FailedPublicationGrab.objects.update_or_create(title=title, authors=authors)  # Record failed search attempt
     raise RuntimeError('Article not found in Scholar.')
 
 
